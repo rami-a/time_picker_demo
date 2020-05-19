cd2eab3190fbe5ade483304a24a48231886c919c
Rami Abou Ghanem <raboughanem@google.com>
2020-05-19 11:53:47 -0400

    Add text scaling to demo

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
 
 void main() {
-//  timeDilation = 2;
   runApp(MyApp());
 }
 
@@ -33,72 +32,94 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TimeOfDay _time = TimeOfDay.now();
-  DateTime _date = DateTime.now();
   bool _isSlow = false;
+  List<bool> _isSelected = [true, false, false];
 
+  double get _textScale {
+    if (_isSelected[1]) return 1.3;
+    if (_isSelected[2]) return 2.0;
+    return 1.0;
+  }
   void _selectTime() async {
-    final TimeOfDay newTime = await showTimePicker(context: context, initialTime: _time);
+    final TimeOfDay newTime = await showTimePicker(
+      context: context,
+      initialTime: _time,
+      builder: (context, child) {
+
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: _textScale),
+          child: child,
+        );
+      }
+    );
     if (newTime != null) {
       setState(() { _time = newTime; });
     }
   }
 
-  void _selectDate() async {
-    final DateTime newDate = await showDatePicker(
-      context: context,
-      initialDate: _date,
-      firstDate: DateTime(2020, 1, 15),
-      lastDate: DateTime(2101),
-    );
-    if (newDate != null) {
-      setState(() { _date = newDate; });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Time Picker Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: _selectTime,
-              child: Text('SELECT TIME'),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Selected time: ${_time.format(context)}',
-            ),
-            SizedBox(height: 16),
-            RaisedButton(
-              onPressed: _selectDate,
-              child: Text('SELECT DATE'),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Selected date: ${DateFormat.yMMMd().format(_date)}',
-            ),
-            SizedBox(height: 16),
-            Row(
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(textScaleFactor: _textScale),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Time Picker Demo'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Slow Motion'),
-                Switch(
-                  value: _isSlow,
-                  onChanged: (value) {
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: _selectTime,
+                  child: Text('SELECT TIME'),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Selected time: ${_time.format(context)}',
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Slow Motion'),
+                    Switch(
+                      value: _isSlow,
+                      onChanged: (value) {
+                        setState(() {
+                          _isSlow = value;
+                          timeDilation = _isSlow ? 5 : 1;
+                        });
+                      },
+                    )
+                  ],
+                ),
+                SizedBox(height: 16),
+                Text('Text scale:'),
+                SizedBox(height: 4),
+                ToggleButtons(
+                  children: <Widget>[
+                    Text('1.0'),
+                    Text('1.3'),
+                    Text('2.0'),
+                  ],
+                  onPressed: (int index) {
                     setState(() {
-                      _isSlow = value;
-                      timeDilation = _isSlow ? 5 : 1;
+                      for (int buttonIndex = 0; buttonIndex < _isSelected.length; buttonIndex++) {
+                        if (buttonIndex == index) {
+                          _isSelected[buttonIndex] = true;
+                        } else {
+                          _isSelected[buttonIndex] = false;
+                        }
+                      }
                     });
                   },
-                )
+                  isSelected: _isSelected,
+                ),
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
