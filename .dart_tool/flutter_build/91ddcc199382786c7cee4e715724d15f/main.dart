976e0c7c0fef43c99664c2fa46347d8f1f35e1cb
Rami Abou Ghanem <raboughanem@google.com>
2020-06-03 10:27:05 -0400

    Build files

---
+++ .dart_tool/flutter_build/91ddcc199382786c7cee4e715724d15f/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:timepickerdemo/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
